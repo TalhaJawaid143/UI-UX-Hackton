d1c37b43999fb5b7ad83b7d16e02255791831c8e
Talha Jawaid <talhajawaid601@gmail.com>
2024-12-09 23:39:29 +0500

    Update DressStyleSection.tsx

--- src/components/shop-page/filters/DressStyleSection.tsx
+++ src/components/shop-page/filters/DressStyleSection.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 import Link from "next/link";
-import { MdKeyboardArrowRight } from "react-icons/md";
 
 type DressStyle = {
   title: string;
@@ -34,26 +27,18 @@ const dressStylesData: DressStyle[] = [
 
 const DressStyleSection = () => {
   return (
-    <Accordion type="single" collapsible defaultValue="filter-style">
-      <AccordionItem value="filter-style" className="border-none">
-        <AccordionTrigger className="text-black font-bold text-xl hover:no-underline p-0 py-0.5">
-          Dress Style
-        </AccordionTrigger>
-        <AccordionContent className="pt-4 pb-0">
-          <div className="flex flex-col text-black/60 space-y-0.5">
-            {dressStylesData.map((dStyle, idx) => (
-              <Link
-                key={idx}
-                href={dStyle.slug}
-                className="flex items-center justify-between py-2"
-              >
-                {dStyle.title} <MdKeyboardArrowRight />
-              </Link>
-            ))}
-          </div>
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+    <section>
+      <h2 className="text-black font-bold text-xl mb-2">Dress Style</h2>
+      <ul className="flex flex-col space-y-2">
+        {dressStylesData.map((dStyle, idx) => (
+          <li key={idx} className="text-black/60">
+            <Link href={dStyle.slug} className="hover:underline">
+              {dStyle.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
