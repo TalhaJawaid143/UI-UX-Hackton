db683863cacdef6542dbfaafc90f1e4b37128d59
Talha Jawaid <talhajawaid601@gmail.com>
2024-12-09 23:40:18 +0500

    Update PriceSection.tsx

--- src/components/shop-page/filters/PriceSection.tsx
+++ src/components/shop-page/filters/PriceSection.tsx
@@ -9,18 +9,17 @@ import { Slider } from "@/components/ui/slider";
 
 const PriceSection = () => {
   return (
-    <Accordion type="single" collapsible defaultValue="filter-price">
+    <Accordion type="single" collapsible defaultValue="">
       <AccordionItem value="filter-price" className="border-none">
         <AccordionTrigger className="text-black font-bold text-xl hover:no-underline p-0 py-0.5">
           Price
         </AccordionTrigger>
         <AccordionContent className="pt-4" contentClassName="overflow-visible">
           <Slider
-            defaultValue={[50, 200]}
+            defaultValue={[50, 100]}
             min={0}
             max={250}
             step={1}
-            label="$"
           />
           <div className="mb-3" />
         </AccordionContent>
