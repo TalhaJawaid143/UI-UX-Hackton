7b483ec687cce885d66c64601687e7b4770a5b60
Talha Jawaid <talhajawaid601@gmail.com>
2024-12-09 07:28:03 +0500

    Update index.tsx

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -105,18 +105,18 @@ const Footer = () => {
             <p className="text-sm text-center sm:text-left text-black/60 mb-4 sm:mb-0 sm:mr-1">
               Shop.co © Made by{" "}
               <Link
-                href="https://github.com/mohammadoftadeh"
+                href="https://github.com"
                 className="text-black font-medium"
               >
-                Mohammad Oftadeh
+                Talha Jawaid
               </Link>
               {", "}
               Designed by{" "}
               <Link
-                href="https://www.figma.com/@hamzauix"
+                href="https://www.figma.com"
                 className="text-black font-medium"
               >
-                Hamza Naeem
+                 Talha Jawaid
               </Link>
             </p>
             <div className="flex items-center">
