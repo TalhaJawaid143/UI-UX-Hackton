82c39e0c4b7c3b297984224a4bde4c406b8e247a
Talha Jawaid <talhajawaid601@gmail.com>
2024-12-09 23:26:37 +0500

    Update LayoutSpacing.tsx

--- src/components/layout/Footer/LayoutSpacing.tsx
+++ src/components/layout/Footer/LayoutSpacing.tsx
@@ -1,14 +1,9 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import React from "react";
 
-const LayoutSpacing = () => {
-  const pathname = usePathname();
-
-  if (!pathname.includes("product")) return;
-
-  return <div className="mb-20 md:mb-0" />;
-};
+const LayoutSpacing = () => 
+  usePathname()?.includes("product") ? <div className="mb-20 md:mb-0" /> : null;
 
 export default LayoutSpacing;
+
