2f3278c3924cfbd6a8e605d77c8e3df2ccdca402
Talha Jawaid <talhajawaid601@gmail.com>
2024-12-10 09:43:18 +0500

    Update AnimatedCounter.tsx

--- src/components/ui/AnimatedCounter.tsx
+++ src/components/ui/AnimatedCounter.tsx
@@ -1,58 +1,17 @@
 "use client";
 
-import {
-  KeyframeOptions,
-  animate,
-  useInView,
-  useIsomorphicLayoutEffect,
-} from "framer-motion";
-import { useRef } from "react";
+import React, { useRef } from "react";
 
 type AnimatedCounterProps = {
   from: number;
   to: number;
-  animationOptions?: KeyframeOptions;
 };
 
-const AnimatedCounter = ({
-  from,
-  to,
-  animationOptions,
-}: AnimatedCounterProps) => {
+const AnimatedCounter = ({ to }: AnimatedCounterProps) => {
   const ref = useRef<HTMLSpanElement>(null);
-  const inView = useInView(ref, { once: true });
 
-  useIsomorphicLayoutEffect(() => {
-    const element = ref.current;
-
-    if (!element) return;
-    if (!inView) return;
-
-    // Set initial value
-    element.textContent = String(from);
-
-    // If reduced motion is enabled in system's preferences
-    if (window.matchMedia("(prefers-reduced-motion)").matches) {
-      element.textContent = String(to);
-      return;
-    }
-
-    const controls = animate(from, to, {
-      duration: 6,
-      ease: "easeOut",
-      ...animationOptions,
-      onUpdate(value) {
-        element.textContent = Number(value.toFixed(0)).toLocaleString();
-      },
-    });
-
-    // Cancel on unmount
-    return () => {
-      controls.stop();
-    };
-  }, [ref, inView, from, to]);
-
-  return <span ref={ref} />;
+  return <span ref={ref}>{to.toLocaleString()}</span>;
 };
 
 export default AnimatedCounter;
+
