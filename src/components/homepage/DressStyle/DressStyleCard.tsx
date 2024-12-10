c5fbba61f2a7b199939bc5932faf731478fee608
Talha Jawaid <talhajawaid601@gmail.com>
2024-12-10 09:38:12 +0500

    Update DressStyleCard.tsx

--- src/components/homepage/DressStyle/DressStyleCard.tsx
+++ src/components/homepage/DressStyle/DressStyleCard.tsx
@@ -1,4 +1,3 @@
-import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
 
@@ -8,18 +7,13 @@ type DressStyleCardProps = {
   className?: string;
 };
 
-const DressStyleCard = ({ title, url, className }: DressStyleCardProps) => {
-  return (
-    <Link
-      href={url}
-      className={cn([
-        "w-full md:h-full rounded-[20px] bg-white bg-top text-2xl md:text-4xl font-bold text-left py-4 md:py-[25px] px-6 md:px-9 bg-no-repeat bg-cover",
-        className,
-      ])}
-    >
-      {title}
-    </Link>
-  );
-};
+const DressStyleCard = ({ title, url, className }: DressStyleCardProps) => (
+  <Link
+    href={url}
+    className={`w-full md:h-full rounded-[20px] bg-white bg-top text-2xl md:text-4xl font-bold text-left py-4 md:py-[25px] px-6 md:px-9 bg-no-repeat bg-cover ${className || ""}`}
+  >
+    {title}
+  </Link>
+);
 
 export default DressStyleCard;
