f76a1f589131026560bf33da252c5a76f995d191
Talha Jawaid <talhajawaid601@gmail.com>
2024-12-10 09:27:06 +0500

    Update BreadcrumbCart.tsx

--- src/components/cart-page/BreadcrumbCart.tsx
+++ src/components/cart-page/BreadcrumbCart.tsx
@@ -1,29 +1,19 @@
 import React from "react";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import Link from "next/link";
 
 const BreadcrumbCart = () => {
   return (
-    <Breadcrumb className="mb-2 sm:mb-6">
-      <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink asChild>
-            <Link href="/">Home</Link>
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbPage>Cart</BreadcrumbPage>
-        </BreadcrumbItem>
-      </BreadcrumbList>
-    </Breadcrumb>
+    <nav className="mb-2 sm:mb-6" aria-label="breadcrumb">
+      <ol className="flex space-x-2">
+        <li>
+          <Link href="/" className="text-blue-600 hover:underline">
+            Home
+          </Link>
+        </li>
+        <span className="text-gray-500">/</span>
+        <li className="text-gray-700 font-medium">Cart</li>
+      </ol>
+    </nav>
   );
 };
 
