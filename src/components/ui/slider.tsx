7d34dd9f9fe8ffd31330da9132e0b8d00950d6ee
Talha Jawaid <talhajawaid601@gmail.com>
2024-12-10 09:46:03 +0500

    Update slider.tsx

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -10,73 +10,29 @@ interface SliderProps
   max: number;
   step?: number;
   defaultValue?: [number, number];
-  label?: string;
 }
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   SliderProps
 >(
-  (
-    {
-      className,
-      min,
-      max,
-      step = 1,
-      defaultValue = [min, max],
-      label,
-      ...props
-    },
-    ref
-  ) => {
-    const [values, setValues] = React.useState<[number, number]>(defaultValue);
-
-    const handleValueChange = (newValues: number[]) => {
-      setValues([newValues[0], newValues[1]]);
-    };
-
+  ({ className, min, max, step = 1, defaultValue = [min, max], ...props }, ref) => {
     return (
-      <div className="w-full relative">
+      <div className="w-full">
         <SliderPrimitive.Root
           ref={ref}
-          className={cn(
-            "relative flex w-full touch-none select-none items-center",
-            className
-          )}
+          className={cn("relative flex w-full items-center", className)}
           min={min}
           max={max}
           step={step}
-          value={values}
-          onValueChange={handleValueChange}
+          defaultValue={defaultValue}
           {...props}
         >
-          <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
-            <SliderPrimitive.Range className="absolute h-full bg-primary" />
+          <SliderPrimitive.Track className="relative h-1 w-full bg-gray-200 rounded-full">
+            <SliderPrimitive.Range className="absolute h-full bg-black" />
           </SliderPrimitive.Track>
-
-          {/* Thumb 1 with Label */}
-          <div
-            className="absolute -translate-x-1/2 -bottom-8 text-xs font-medium px-2 py-1 rounded z-10"
-            style={{
-              left: `${((values[0] - min) / (max - min)) * 100}%`,
-            }}
-          >
-            {label}
-            {values[0]}
-          </div>
-          <SliderPrimitive.Thumb className="relative block h-4 w-4 rounded-full border border-primary/50 bg-black shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
-
-          {/* Thumb 2 with Label */}
-          <div
-            className="absolute -translate-x-1/2 -bottom-8 text-xs font-medium px-2 py-1 rounded z-10"
-            style={{
-              left: `${((values[1] - min) / (max - min)) * 100}%`,
-            }}
-          >
-            {label}
-            {values[1]}
-          </div>
-          <SliderPrimitive.Thumb className="relative block h-4 w-4 rounded-full border border-primary/50 bg-black shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+          <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full bg-black" />
+          <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full bg-black" />
         </SliderPrimitive.Root>
       </div>
     );
