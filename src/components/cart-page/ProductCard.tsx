2912af27a00e0fd36d80185710a8afb0ba8d686c
Talha Jawaid <talhajawaid601@gmail.com>
2024-12-10 09:27:31 +0500

    Update ProductCard.tsx

--- src/components/cart-page/ProductCard.tsx
+++ src/components/cart-page/ProductCard.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import React from "react";
 import { PiTrashFill } from "react-icons/pi";
 import Image from "next/image";
 import Link from "next/link";
 import CartCounter from "@/components/ui/CartCounter";
-import { Button } from "../ui/button";
 import {
   addToCart,
   CartItem,
@@ -31,7 +29,7 @@ const ProductCard = ({ data }: ProductCardProps) => {
           src={data.srcUrl}
           width={124}
           height={124}
-          className="rounded-md w-full h-full object-cover hover:scale-110 transition-all duration-500"
+          className="rounded-md w-full h-full object-cover"
           alt={data.name}
           priority
         />
@@ -44,72 +42,35 @@ const ProductCard = ({ data }: ProductCardProps) => {
           >
             {data.name}
           </Link>
-          <Button
-            variant="ghost"
-            size="icon"
-            className="h-5 w-5 md:h-9 md:w-9"
-            onClick={() =>
-              dispatch(
-                remove({
-                  id: data.id,
-                  attributes: data.attributes,
-                  quantity: data.quantity,
-                })
-              )
-            }
-          >
-            <PiTrashFill className="text-xl md:text-2xl text-red-600" />
-          </Button>
+         
         </div>
-        <div className="-mt-1">
+        <div className="mb-1">
           <span className="text-black text-xs md:text-sm mr-1">Size:</span>
           <span className="text-black/60 text-xs md:text-sm">
-            {data.attributes[0]}
+            {data.attributes?.[0]}
           </span>
         </div>
         <div className="mb-auto -mt-1.5">
           <span className="text-black text-xs md:text-sm mr-1">Color:</span>
           <span className="text-black/60 text-xs md:text-sm">
-            {data.attributes[1]}
+            {data.attributes?.[1]}
           </span>
         </div>
         <div className="flex items-center flex-wrap justify-between">
           <div className="flex items-center space-x-[5px] xl:space-x-2.5">
-            {data.discount.percentage > 0 ? (
-              <span className="font-bold text-black text-xl xl:text-2xl">
-                {`$${Math.round(
-                  data.price - (data.price * data.discount.percentage) / 100
-                )}`}
-              </span>
-            ) : data.discount.amount > 0 ? (
-              <span className="font-bold text-black text-xl xl:text-2xl">
-                {`$${data.price - data.discount.amount}`}
-              </span>
-            ) : (
-              <span className="font-bold text-black text-xl xl:text-2xl">
-                ${data.price}
-              </span>
-            )}
-            {data.discount.percentage > 0 && (
+            <span className="font-bold text-black text-xl xl:text-2xl">
+              {data.discount?.percentage > 0
+                ? `$${Math.round(
+                    data.price - (data.price * data.discount.percentage) / 100
+                  )}`
+                : data.discount?.amount > 0
+                ? `$${data.price - data.discount.amount}`
+                : `$${data.price}`}
+            </span>
+            {(data.discount?.percentage > 0 || data.discount?.amount > 0) && (
               <span className="font-bold text-black/40 line-through text-xl xl:text-2xl">
                 ${data.price}
               </span>
-            )}
-            {data.discount.amount > 0 && (
-              <span className="font-bold text-black/40 line-through text-xl xl:text-2xl">
-                ${data.price}
-              </span>
-            )}
-            {data.discount.percentage > 0 ? (
-              <span className="font-medium text-[10px] xl:text-xs py-1.5 px-3.5 rounded-full bg-[#FF3333]/10 text-[#FF3333]">
-                {`-${data.discount.percentage}%`}
-              </span>
-            ) : (
-              data.discount.amount > 0 && (
-                <span className="font-medium text-[10px] xl:text-xs py-1.5 px-3.5 rounded-full bg-[#FF3333]/10 text-[#FF3333]">
-                  {`-$${data.discount.amount}`}
-                </span>
-              )
             )}
           </div>
           <CartCounter
